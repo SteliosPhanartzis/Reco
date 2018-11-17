c2ee89af14aacdc0c20ce5623ff2c08aa74aa0c9
Stelios <steliftw@gmail.com>
2018-11-16 20:14:22 -0500

    Add files via upload
    Running mock up for web app.

---
+++ documentLoad.js
@@ -0,0 +1,21 @@
+function hamburgler(x){
+    x.classList.toggle("change");
+    var dropdown = document.getElementById("myDropdown");
+	//toggle showing dropdown
+  	dropdown.classList.toggle("show");
+}
+
+function pageContentsOffset(){
+		window_height = $(window).height();
+		navOffset = document.defaultView.getComputedStyle(document.getElementById('navbar-top'), "").getPropertyValue("height");
+		dropdown = document.getElementById('myDropdown');
+		//sizing
+		dropdown.style.height = (window_height - parseInt(navOffset)) + "px";
+	    dropdown.style.top = navOffset;
+	    document.getElementsByClassName('bodyBack')[0].style.paddingTop = navOffset;
+}
+
+function resizeElements(){
+	pageContentsOffset();
+}
+pageContentsOffset();
