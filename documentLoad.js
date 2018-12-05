dcbca363d94609a097846c04ca12ab15ce660ee6
SteliosPhanartzis <stelios.phan@gmail.com>
2018-12-05 01:26:47 -0500

    Added gmap

--- documentLoad.js
+++ documentLoad.js
@@ -12,7 +12,8 @@ function pageContentsOffset(){
 		//sizing
 		dropdown.style.height = (window_height - parseInt(navOffset)) + "px";
 	    dropdown.style.top = navOffset;
-	    document.getElementsByClassName('bodyBack')[0].style.paddingTop = navOffset;
+	    document.getElementById('map').style.paddingTop = navOffset;
+	    //document.getElementsByClassName('bodyBack')[0].style.paddingTop = navOffset;
 }
 
 function resizeElements(){
